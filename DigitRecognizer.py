11b9f9932a22af307798de9e71e617f007b27749
Tobias <tobias_maier99@gmx.de>
2016-04-05 05:01:27 +0200

    new function DigitRecognizer.crossValidate()
    this function splits the training set into training set and
    crossvalidation set, runs the solving on the crossvalidation set and
    checks the solution against the known right answers
    
    returns the percentage of right answers and the list with boolean values

--- DigitRecognizer.py
+++ DigitRecognizer.py
@@ -85,11 +85,40 @@ class DigitRecognizer:
                    header = '"ImageId", "Label"', comments = '')
 
 
+    def crossValidate(self, splitAt = 0.7):
+        """
+        this function takes the training set where the right answer is known,
+        splits it 70/30, takes the bigger part as the normal training set and
+        the smaller part as test set. afterwards it compares the answer from
+        the solver with the known right answer
+        """
+
+        #load the training set from the csv file
+        data = readCsvData(self.trainFile)
+
+        #define where to split the dataset. Rounds down
+        splitter = (int)(splitAt * len(data))
+
+        trainData = data[:splitter]
+        validationData = data[splitter:]
+
+        # run the solving algorythm
+        # use the validation set without the solution in the first row
+        self.solver.loadData(trainData, validationData[:,1:])
+        self.sol = self.solver.solve()
+        self.validationData = validationData
+
+        # check the solution
+        rightSol = validationData[:,0] == self.sol[:,1]
+        rightPercent = np.bincount(rightSol)[1] / len(rightSol)
+        return rightPercent, rightSol
+
+
     def timeit(self, funcName):
         """
         this function returns the time it takes to run another function
 
-        I do this internally instead of the python package timeit because 
+        I do this internally instead of the python package timeit because
         like this we can preload the data and just run the functions repeatedly
         on this allready loaded data
         """
